93abd6b748d64a680edebbd9dd6dc02402087384
Daniel <dddanielliu@gmail.com>
2026-06-25 21:34:00 +0800

    add cache

--- backend/src/main/java/com/example/springtest/SpringtestApplication.java
+++ backend/src/main/java/com/example/springtest/SpringtestApplication.java
@@ -2,8 +2,10 @@ package com.example.springtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringtestApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/example/springtest/product/services/UpdateProductService.java
+++ backend/src/main/java/com/example/springtest/product/services/UpdateProductService.java
@@ -2,6 +2,7 @@ package com.example.springtest.product.services;
 
 import java.util.Optional;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -23,6 +24,7 @@ public class UpdateProductService implements Command<UpdateProductCommand, Produ
     }
 
     @Override
+    @CacheEvict(value="productCache", key="#command.getId()")
     public ResponseEntity<ProductDTO> execute(UpdateProductCommand command) {
         Optional<Product> productOptional = productRepository.findById(command.getId());
         if (productOptional.isPresent()) {
