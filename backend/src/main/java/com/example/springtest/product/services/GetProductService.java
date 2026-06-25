1fc5c99d52f08f69d5ff76fd81d040bb78162d59
Daniel <dddanielliu@gmail.com>
2026-06-25 21:26:27 +0800

    add logging

--- backend/src/main/java/com/example/springtest/product/services/GetProductService.java
+++ backend/src/main/java/com/example/springtest/product/services/GetProductService.java
@@ -2,6 +2,8 @@ package com.example.springtest.product.services;
 
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ import com.example.springtest.product.model.ProductDTO;
 @Service
 public class GetProductService implements Query<Integer, ProductDTO>{
     private final ProductRepository productRepository;
+
+    private static final Logger logger = LoggerFactory.getLogger(GetProductService.class);
     
     public GetProductService(ProductRepository productRepository) {
         this.productRepository = productRepository;
@@ -21,6 +25,9 @@ public class GetProductService implements Query<Integer, ProductDTO>{
 
     @Override
     public ResponseEntity<ProductDTO> execute(Integer input) {
+
+        logger.info("Executing {} input : {}", getClass(), input);
+
         // account for null value if db can't find it
         Optional<Product> productOptional = productRepository.findById(input);
         if (productOptional.isPresent()){
