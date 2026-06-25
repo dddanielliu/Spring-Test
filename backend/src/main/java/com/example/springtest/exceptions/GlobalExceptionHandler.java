8eebc0d3a159d438d2579f8338f6fcf48c9c2230
Daniel <dddanielliu@gmail.com>
2026-06-25 11:03:52 +0800

    use annotations for GlobalExceptionHandler

--- backend/src/main/java/com/example/springtest/exceptions/GlobalExceptionHandler.java
+++ backend/src/main/java/com/example/springtest/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,9 @@
 package com.example.springtest.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.example.springtest.product.model.ErrorResponse;
@@ -10,7 +11,9 @@ import com.example.springtest.product.model.ErrorResponse;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public ErrorResponse handleProductNotFoundException(ProductNotFoundException exception) {
+        return new ErrorResponse(exception.getMessage());
     }
 }
