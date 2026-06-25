c2bc2a043dbc75c5d6b47d89122f7f611e54d84c
Daniel <dddanielliu@gmail.com>
2026-06-25 11:03:26 +0800

    use GlobalExceptionHandler

--- backend/src/main/java/com/example/springtest/product/ProductController.java
+++ backend/src/main/java/com/example/springtest/product/ProductController.java
@@ -2,10 +2,8 @@ package com.example.springtest.product;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,8 +11,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.springtest.exceptions.ProductNotFoundException;
-import com.example.springtest.product.model.ErrorResponse;
 import com.example.springtest.product.model.Product;
 import com.example.springtest.product.model.ProductDTO;
 import com.example.springtest.product.model.UpdateProductCommand;
@@ -70,10 +66,5 @@ public class ProductController {
     @DeleteMapping("/product/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
         return deleteProductService.execute(id);
-    }
-
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleProductNotFoundException(ProductNotFoundException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(exception.getMessage()));
     }
 }
